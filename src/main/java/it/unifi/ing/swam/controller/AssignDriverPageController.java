30aad0c8adb7252e8920dccecfd02e49de9dfeb0
iltruma <casini.cosimo@gmail.com>
2017-07-05 17:12:51 +0200

    Re-fix scopes

--- src/main/java/it/unifi/ing/swam/controller/AssignDriverPageController.java
+++ src/main/java/it/unifi/ing/swam/controller/AssignDriverPageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 
 import it.unifi.ing.swam.bean.ConversationBean;
