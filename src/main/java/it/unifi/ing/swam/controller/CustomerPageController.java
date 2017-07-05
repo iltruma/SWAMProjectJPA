95b058138e04533dbb9312442368daa8749c7c2a
Riccardo Grazzi <rikkajounin@gmail.com>
2017-07-05 09:40:53 +0200

    filled customersPageController

--- src/main/java/it/unifi/ing/swam/controller/CustomerPageController.java
+++
@@ -1,7 +0,0 @@
-package it.unifi.ing.swam.controller;
-
-public class CustomerPageController extends BasicController{
-
-	
-	
-}
---
+++ src/main/java/it/unifi/ing/swam/controller/CustomersPageController.java
@@ -0,0 +1,39 @@
+package it.unifi.ing.swam.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import it.unifi.ing.swam.dao.CustomerDao;
+import it.unifi.ing.swam.model.Customer;
+import it.unifi.ing.swam.model.User;
+
+public class CustomersPageController extends BasicController{
+	
+	@Inject 
+	private CustomerDao customerDao;
+	
+	private List<User> customers;
+
+
+	@PostConstruct
+	protected void initCustomersPage(){
+		if(userSession.getUser().isOperator()) {
+			throw new IllegalArgumentException("you cant view this page");
+		}
+		List<Customer> results = customerDao.findByOperator(userSession.getUser());
+		customers = new ArrayList<User>();
+		
+		for(Customer c: results){
+			customers.add(c.getOwner());
+		}
+		
+	}
+	
+	public List<User> getCustomers() {
+		return customers;
+	}
+	
+}
